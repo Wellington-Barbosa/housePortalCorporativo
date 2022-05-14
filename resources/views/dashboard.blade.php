69af02ab5db5104fa04baababcd8fc60f64a8b7d
Wellington Barbosa <wellington_ueg@hotmail.com>
2022-05-14 01:48:51 -0300

    - Projeto criado com as funcionalidades básicas de login. - Implementação do template AdminLTe 3. - Implementação da tela inicial do portal.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <button class="btn btn-success col col-12" disabled="disabled"><h1>Bem Vindo ao Sistema Corporativo</h1></button>
+@stop
+
+@section('content')
+
+
+
+@stop
